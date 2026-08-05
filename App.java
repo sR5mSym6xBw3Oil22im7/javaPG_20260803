e6c8b96701bdcb7d2cc2ff75617ab4078d1b19f4
LAPTOP-SNQFGMRT\academia <g6363dms@outlook.jp>
2026-08-05 15:07:31 +0900

    Added /todos HTML endpoint returning a todo list.

--- App.java
+++ App.java
@@ -1,74 +1,65 @@
-// HTTP サーバーを使うための部品です。
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
-// 返事の本文を書き出すための部品です。
+import java.io.IOException;
 import java.io.OutputStream;
-// 8080 番ポートを指定するための部品です。
 import java.net.InetSocketAddress;
-// URL の文字を元に戻すための部品です。
 import java.net.URLDecoder;
-// 文字を UTF-8 に変えるための部品です。
 import java.nio.charset.StandardCharsets;
-// App という名前のクラスです。
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
-    // 最初に動く場所です。
     public static void main(String[] args) throws Exception {
-        // 8080 番ポートで待ち受けるサーバーを作ります。
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        // / に来たアクセスの処理を登録します。
+
         server.createContext("/", exchange -> {
-            // 今きたリクエストのパスを取り出します。
             String path = exchange.getRequestURI().getPath();
-            // 返す文章を入れる箱です。
             String message;
-            // 返す HTTP ステータスを入れる箱です。
             int statusCode;
-            // /hello のときの処理です。
+            String contentType = "text/plain; charset=UTF-8";
+
             if (path.equals("/hello")) {
-                // URL のクエリをそのまま取ります。
                 String query = exchange.getRequestURI().getRawQuery();
-                // 確認のために今の query を表示します。
                 System.out.println("query = " + query);
-                // いったんゲストにしておきます。
                 String name = "ゲスト";
-                // query があるときだけ、name= の後ろを切り出します。
                 if (query != null) {
                     name = URLDecoder.decode(query.substring("name=".length()), StandardCharsets.UTF_8);
                 }
-                // 取り出した名前で挨拶を作ります。
                 message = "こんにちは、" + name + "さん！";
-                // 成功として返します。
                 statusCode = 200;
             } else if (path.equals("/menu")) {
-                // 今日の定食を返します。
-                message = "今日の定食はカレー";
-                // 成功として返します。
+                message = "今日の特別メニューはカレー";
                 statusCode = 200;
             } else if (path.equals("/bye")) {
-                // さようならを返します。
-                message = "さようなら！";
-                // 成功として返します。
+                message = "さようなら";
                 statusCode = 200;
+            } else if (path.equals("/todos")) {
+                // 追加: Todo を入れる List を用意します。
+                List<String> todos = Arrays.asList("牛乳を買う", "卵を買う", "パンを買う");
+                // 追加: HTML を入れる変数 html に、<ul><li>...</li></ul> を組み立てます。
+                String html = "<ul>";
+                for (String todo : todos) {
+                    html += "<li>" + todo + "</li>";
+                }
+                html += "</ul>";
+                message = html;
+                statusCode = 200;
+                // 追加: /todos のときだけ HTML として返します。
+                contentType = "text/html; charset=UTF-8";
             } else {
-                // 見つからないときの文を返します。
                 message = "ページが見つかりません";
-                // 見つからないときの番号を返します。
                 statusCode = 404;
             }
-            // 文字の文章をバイト列に変えます。
+
             byte[] body = message.getBytes(StandardCharsets.UTF_8);
-            // 返事の種類を設定します。
-            exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=UTF-8");
-            // ステータスと本文の長さを伝えます。
+            exchange.getResponseHeaders().set("Content-Type", contentType);
             exchange.sendResponseHeaders(statusCode, body.length);
-            // 返事の本文を書き込みます。
             try (OutputStream os = exchange.getResponseBody()) {
-                // 本文を送ります。
                 os.write(body);
             }
         });
-        // サーバーを動かし始めます。
+
         server.start();
-        // プログラムをすぐ終わらせないように止めておきます。
         Thread.currentThread().join();
     }
 }
